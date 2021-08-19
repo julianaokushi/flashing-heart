617cd0499e97c4983c73e01fc133f1070b180faa
Juliana Okushi <julianabsp@hotmail.com>
2021-08-18 21:26:23 -0300

    inserçao de frase

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
 basic.forever(function () {
+    basic.showString("Amoreco")
     basic.showLeds(`
-        . . . . .
         . # . # .
+        # # # # #
         # # # # #
         . # # # .
         . . # . .
         `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("Te amo")
     basic.showLeds(`
         . . . . .
         . . . . .
